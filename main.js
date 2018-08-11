7a2030ed56e875dcb86c1bb28374587da73c8fc8
Unknown <internetz@eclipso.de>
2018-08-11 14:51:19 +0200

    upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const wait = require('wait-for-stuff');
+class Blocker {
+  constructor() {this.block ={'active': true}}
+  release() {this.block.active = false}
+  wait() {wait.for.value(this.block, 'active', false)}
+}
+module.exports = Blocker
